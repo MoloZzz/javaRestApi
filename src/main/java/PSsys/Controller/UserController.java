842f8fbaee9355cf658cb392cbda929652ede4e0
MoloZzz <mega_oleksii@ukr.net>
2024-05-23 11:45:09 +0300

    feat: update structure

--- src/main/java/PSsys/Controller/UserController.java
+++ src/main/java/PSsys/Controller/UserController.java
@@ -1,4 +1,44 @@
 package PSsys.Controller;
 
-public class UserController {
+import PSsys.Configuration.DatabaseConnection;
+import PSsys.DAO.impl.UserDaoImpl;
+import PSsys.Service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/users/*")
+public class UserController extends HttpServlet {
+
+    private UserService userService;
+
+    @Override
+    public void init() throws ServletException {
+        UserDaoImpl userDao = new UserDaoImpl(DatabaseConnection.getInstance().getConnection());
+        this.userService = new UserService(userDao);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Обробка GET запитів
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Обробка POST запитів
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Обробка PUT запитів
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Обробка DELETE запитів
+    }
 }
--- src/main/java/PSsys/Service/UserService.java
+++ src/main/java/PSsys/Service/UserService.java
@@ -1,4 +1,38 @@
 package PSsys.Service;
 
+import PSsys.DAO.UserDao;
+import PSsys.Model.User;
+
+import java.util.List;
+
 public class UserService {
+    private UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public User findById(int id) {
+        return userDao.findById(id);
+    }
+
+    public User findByUsername(String username) {
+        return userDao.findByUsername(username);
+    }
+
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    public void update(User user) {
+        userDao.update(user);
+    }
+
+    public void delete(int id) {
+        userDao.delete(id);
+    }
 }
